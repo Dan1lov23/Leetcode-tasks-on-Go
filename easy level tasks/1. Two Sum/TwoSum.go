91de1c3649de99c785b8cce1db189c519ee9047f
kitik <svy1dan@gmail.com>
2026-03-11 18:15:12 +0300

    add new task

---
+++ easy level tasks/1. Two Sum/TwoSum.go
@@ -0,0 +1,63 @@
+/* Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+
+You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+You can return the answer in any order.
+
+
+
+Example 1:
+
+Input: nums = [2,7,11,15], target = 9
+Output: [0,1]
+Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+Example 2:
+
+Input: nums = [3,2,4], target = 6
+Output: [1,2]
+Example 3:
+
+Input: nums = [3,3], target = 6
+Output: [0,1]
+
+
+Constraints:
+
+2 <= nums.length <= 104
+-109 <= nums[i] <= 109
+-109 <= target <= 109
+Only one valid answer exists.
+
+
+Follow-up: Can you come up with an algorithm that is less than O(n2) time complexity?
+
+*/
+
+package main
+
+import "fmt"
+
+func TwoSum(nums []int, target int) []int {
+
+	for i := 0; i < len(nums); i++ {
+		if i + 1 <= len(nums) {
+			for j := 0; j < len(nums); j ++ {
+				if j + 1 <= len(nums) {
+					if nums[i] + nums[j] == target && i != j {
+						return []int{i, j}
+					}
+				}
+			}
+		}
+	}
+
+	return nil
+
+}
+
+func main() {
+
+	result := TwoSum([]int{3, 2, 3}, 6)
+	fmt.Println(result)
+
+}
